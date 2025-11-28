6d774b6a0479694bd088c973a5749bd1caac9a5e
Militao36 <matheusmoreira3693@gmail.com>
2025-11-28 14:47:42 -0300

    fix

--- api/api.ts
+++ api/api.ts
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://telemetry-telemetry-api.qltz9y.easypanel.host/api/v1",
-  headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZEVtcHJlc2EiOiJhZjhmYjIzNS1lYThmLTQ3MGMtYThmYy1hNzFkYjg3ZDgzNjciLCJpZFVzZXIiOiI4YzczODNjZi1mNTcwLTQ0NzEtODI1Ny1mZDIxOGUxZGQ3YTUiLCJpYXQiOjE3NjQwNzAzMDYsImV4cCI6MTc2NjY2MjMwNn0.HvQWx5nrN7K_RnpMgvbi4ZN4x_SFQTgD7zLZbdzwD5E`,
-  },
 });
 
 api.interceptors.request.use((config) => {
