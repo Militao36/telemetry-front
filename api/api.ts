a6a002ce3cbb9a355927c30a4873b3233593a3ef
Militao36 <matheusmoreira3693@gmail.com>
2025-12-06 14:45:32 -0300

    fix

--- api/api.ts
+++ api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333/api/v1",
+  baseURL: "http://telemetry-telemetry-api.qltz9y.easypanel.host/api/v1",
 });
 
 api.interceptors.request.use((config) => {
